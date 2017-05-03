3436829eb485184061d908026d8831e35db67ead
rahyan <rahyan.azin@gmail.com>
2017-05-03 00:11:54 -0300

    SPRINT 1
    Commit do primeiro sprint.

---
+++ RAHYAN_QUESTAO8/src/main/java/RAHYAN_QUESTAO8/RAHYAN_QUESTAO8/Biblioteca.java
@@ -0,0 +1,18 @@
+package RAHYAN_QUESTAO8.RAHYAN_QUESTAO8;
+
+import java.util.ArrayList;
+
+public class Biblioteca {
+	
+	ArrayList<Livro> bancoLivros = new ArrayList<Livro> ();	
+	ArrayList<Usuario> bancoUsuarios = new ArrayList<Usuario> ();
+	
+	public int numeroUsuarios(){
+		return bancoUsuarios.size();
+	}
+	
+	public Bibliotecaria contratarBibliotecaria(){
+		return new Bibliotecaria(this);
+	}
+	
+}
---
+++ RAHYAN_QUESTAO8/src/main/java/RAHYAN_QUESTAO8/RAHYAN_QUESTAO8/Livro.java
@@ -0,0 +1,8 @@
+package RAHYAN_QUESTAO8.RAHYAN_QUESTAO8;
+
+public class Livro {
+	String nome;
+	int status; //0: disponível; 1: indisponível; 2: extraviado
+	int diasQueFaltamParaSerDevoldido;
+}
+
